ef93bf75728cf833cc39856321087a04492de4ac
Brandon Miller <Brandon@MacBook-Pro.local>
2022-11-23 13:49:28 -0800

    added prettier rules, fixed eslint issue with react import
    Co-authored-by: Brandon Miller <92418658+bmiller1881@users.noreply.github.com>
    Co-authored-by: Jeffery Richardson <104876722+jrichardson-rn@users.noreply.github.com>
    Co-authored-by: Melanie Forbes <50688150+mforbz12@users.noreply.github.com>
    Co-authored-by: Elise McConnell<112969060+enmcco@users.noreply.github.com>
    Co-authored-by: Emily Paine<22458780+erpaine@users.noreply.github.com>

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Input from '../components/Input';
 import { useNavigate } from 'react-router-dom';
--- server/models/databaseModels.ts
+++ server/models/databaseModels.ts
@@ -1,15 +1,16 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 
-const PG_URI = 'postgres://nOObs:testallcaps@dbhive.cxjwyi85ug6q.us-east-1.rds.amazonaws.com/postgres'
+const PG_URI =
+  'postgres://nOObs:testallcaps@dbhive.cxjwyi85ug6q.us-east-1.rds.amazonaws.com/postgres';
 
 const pool = new Pool({
-    connectionString: PG_URI,
-})
+  connectionString: PG_URI,
+});
 
 const db = {
-    query: (text : string, params? : Array<string>) =>  {
-        return pool.query(text, params);
-    }
-}
+  query: (text: string, params?: Array<string>) => {
+    return pool.query(text, params);
+  },
+};
 
 export default db;
