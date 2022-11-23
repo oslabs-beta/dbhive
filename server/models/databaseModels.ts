5d6bcec48c1c1d97acb4f69e1ffc6f99d7c7f23d
Emily Paine <22458780+erpaine@users.noreply.github.com>
2022-11-23 10:37:06 -0800

    set up server
    Co-authored-by: Brandon Miller <92418658+bmiller1881@users.noreply.github.com>
    Co-authored-by: Jeffery Richardson <104876722+jrichardson-rn@users.noreply.github.com>
    Co-authored-by: Melanie Forbes <50688150+mforbz12@users.noreply.github.com>
    Co-authored-by: Elise McConnell<112969060+enmcco@users.noreply.github.com>

---
+++ server/models/databaseModels.ts
@@ -0,0 +1,15 @@
+import {Pool} from 'pg';
+
+const PG_URI = 'postgres://nOObs:testallcaps@dbhive.cxjwyi85ug6q.us-east-1.rds.amazonaws.com/postgres'
+
+const pool = new Pool({
+    connectionString: PG_URI,
+})
+
+const db = {
+    query: (text, params?, callback?) => :any {
+        return pool.query(text, params, callback);
+    }
+}
+
+export default db;
