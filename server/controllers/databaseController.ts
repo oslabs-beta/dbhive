56af50e09bbf55349170d54744584055f7af8b26
Melanie Forbes <mforbes12@gmail.com>
2022-11-25 11:29:13 -0800

    updated stats being sent to front end. Touched dbController and metricApi
    Co-authored-by: Brandon Miller <92418658+bmiller1881@users.noreply.github.com>
    Co-authored-by: Jeffery Richardson <104876722+jrichardson-rn@users.noreply.github.com>
    Co-authored-by: Melanie Forbes <50688150+mforbz12@users.noreply.github.com>
    Co-authored-by: Elise McConnell<112969060+enmcco@users.noreply.github.com>
    Co-authored-by: Emily Paine<22458780+erpaine@users.noreply.github.com>

--- server/controllers/databaseController.ts
+++ server/controllers/databaseController.ts
@@ -2,27 +2,86 @@ import db from '../models/databaseModels';
 import { Request, Response, NextFunction } from 'express';
 
 interface DatabaseController {
-  connection: (req: Request, res: Response, next: NextFunction) => void;
+  queryTimes: (req: Request, res: Response, next: NextFunction) => void;
+  numOfRows: (req: Request, res: Response, next: NextFunction) => void;
+  topCalls: (req: Request, res: Response, next: NextFunction) => void;
+  dbStats: (req: Request, res: Response, next: NextFunction) => void;
 }
 
 const databaseController: DatabaseController = {
-  connection: async (req, res, next) => {
-    console.log('Hello');
-    const queryString = 'SELECT query, mean_exec_time FROM pg_stat_statements';
+  //this method provides average execution times for all queries
+  queryTimes: async (req, res, next) => {
+    //console.log('Hello');
+    //const queryString = 'SELECT query, mean_exec_time FROM pg_stat_statements';
+    //this query provides query times for all queries where the query was 'select *'
+    const queryString = "select query, mean_exec_time from pg_stat_statements where query like '%SELECT * %';"
     let newQuery;
     try {
       newQuery = await db.query(queryString);
-      console.log(newQuery);
-      res.locals.result = newQuery.rows;
+      //console.log(newQuery);
+      res.locals.result = {times: newQuery.rows};
       return next();
     } catch (error) {
       return next({
-        log: `Error caught in databaseController.connection ${error}`,
+        log: `Error caught in databaseController.queryTimes ${error}`,
         status: 400,
         message: `Error has occured in databaseController.connection. ERROR: ${error}`,
       });
     }
   },
+
+  //this method returns the number of rows that a given query touches, where the number is greater than 10
+  numOfRows: async (req, res, next) => {
+    const queryString = "select query, rows from pg_stat_statements where rows > 10";
+    let quantOfRows;
+    try {
+      quantOfRows = await db.query(queryString);
+      //console.log(quantOfRows);
+      res.locals.result.numOfRows = quantOfRows.rows;
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error caught in databaseController.numOfRows ${error}`,
+        status: 400,
+        message: `Error has occured in databaseController.numOfRows. ERROR: ${error}`
+      });
+    }
+  },
+  //this method will provide the top 5 most frequently called queries
+  topCalls: async (req, res, next) => {
+    const queryString = 'select query, calls from pg_stat_statements order by calls desc limit 5';
+    let callFrequency;
+    try {
+      callFrequency = await db.query(queryString);
+      //console.log(callFrequency);
+      res.locals.result.topCalls = callFrequency.rows;
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error caught in databaseController.mostFreqCalls ${error}`,
+        status: 400,
+        message: `Error has occured in databaseController.mostFreqCalls. ERROR: ${error}`
+      })
+    }
+  },
+  //this method pulls the database-wide statistics for a provided database
+  dbStats: async (req, res, next) => {
+    //postgres can be turned into a variable that will represent the requested db
+    const queryString = "select * from pg_stat_database where datname = 'postgres'";
+    let dbOverview;
+    try {
+      dbOverview = await db.query(queryString);
+      res.locals.result.dbStats = dbOverview.rows;
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error caught in databaseController.dbStats ${error}`,
+        status: 400,
+        message: `Error has occured in databaseController.dbStats. ERROR: ${error}`
+      })
+    }
+  }
+
 };
 
 export default databaseController;
--- server/routes/metricApi.ts
+++ server/routes/metricApi.ts
@@ -8,15 +8,14 @@ const router = express.Router();
 //   res.status(200).json({log: 'router working'});
 // })
 
-// router to store informaton from setup page 
+// router to store informaton from setup page
 router.post('/api/placeholder', userController.setup, (req, res) => {
   return res.status(200).json(res.locals);
 })
 
-
- router.get('/querytimes', databaseController.connection, (req,res) =>{
-   res.status(200).json(res.locals.result)
- })
+router.get('/querytimes', databaseController.queryTimes, databaseController.numOfRows, databaseController.topCalls, databaseController.dbStats, (req,res) =>{
+  res.status(200).json(res.locals.result)
+})
 
 export default router;
 
