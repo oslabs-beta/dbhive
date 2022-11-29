3466227e181621793dd15497f073bcd30466ce99
Brandon Miller <Brandon@MacBook-Pro.local>
2022-11-29 08:52:25 -0800

    added sample-data .zip for dev db

--- server/controllers/setupController.ts
+++ server/controllers/setupController.ts
@@ -6,24 +6,27 @@ interface SetupController {
   create: (req: Request, res: Response, next: NextFunction) => void;
 }
 
+export let dbUri = 'empty db uri';
+
 const setupController: SetupController = {
   create: (req, res, next) => {
-    // NOTICE!!! Change to unique secret for best security
-    const secretKey = 'g<Q2dpoJ[S1=9~$';
+    // // NOTICE!!! Change to unique secret for best security
+    // const secretKey = 'g<Q2dpoJ[S1=9~$';
 
-    // Encrypt
-    const ciphertext = AES.encrypt(
-      JSON.stringify(req.body),
-      secretKey
-    ).toString();
+    // // Encrypt
+    // const ciphertext = AES.encrypt(
+    //   JSON.stringify(req.body),
+    //   secretKey
+    // ).toString();
 
-    // Decrypt
-    const bytes = AES.decrypt(ciphertext, secretKey);
-    const originalText = bytes.toString(CryptoJS.enc.Utf8);
+    // // Decrypt
+    // const bytes = AES.decrypt(ciphertext, secretKey);
+    // const originalText = bytes.toString(CryptoJS.enc.Utf8);
 
-    console.log('Original:', req.body);
-    console.log('Encrypted:', ciphertext);
-    console.log('Decrypted:', JSON.parse(originalText));
+    // console.log('Original:', req.body);
+    // console.log('Encrypted:', ciphertext);
+    // console.log('Decrypted:', JSON.parse(originalText));
+    dbUri = req.body;
 
     return next();
   },
