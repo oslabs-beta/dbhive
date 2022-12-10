9b40c1b8d8de4ce12a51d69be1e28ed4453be354
Brandon Miller <Brandon@MacBook-Pro.local>
2022-12-10 10:36:24 -0800

    reset clientMode.ts back to production settings

--- client/clientMode.ts
+++ client/clientMode.ts
@@ -4,7 +4,7 @@ import { UserData } from './clientTypes';
 toggleDashboardAuth default = true, ex. production mode
 toggleDashboardAuth = false for streamlined development within dashboard
 */
-export const toggleDashboardAuth = false;
+export const toggleDashboardAuth = true;
 
 /*
 seedDBs default = [], ex. production mode
@@ -32,17 +32,4 @@ ex.
     ]
 */
 
-export const seedDBs: UserData['dbs'] = [
-  {
-    nickname: 'dbTest',
-    uri: 'postgres://dbhive:teamawesome@dbhive-test.crqqpw0ueush.us-west-2.rds.amazonaws.com:5432/postgres',
-  },
-  {
-    nickname: 'dbTest2',
-    uri: 'postgres://n00bs:testallcaps@dbhive.cxjwyi85ug6q.us-east-1.rds.amazonaws.com:5432/postgres',
-  },
-  {
-    nickname: 'subify',
-    uri: 'postgres://avpneekp:5fsMVQDkJ7HCwrlILZCF7UhKklrdJ1OI@heffalump.db.elephantsql.com/avpneekp',
-  },
-];
+export const seedDBs: UserData['dbs'] = [];
