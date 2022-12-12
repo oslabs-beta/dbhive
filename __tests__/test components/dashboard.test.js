db1e3cc4737395dca7473a73a8b44b21b755c177
Melanie Forbes <mforbes12@gmail.com>
2022-12-12 13:51:15 -0800

    added testId to login and signup, updated tests
    Co-authored-by: Brandon Miller <92418658+bmiller1881@users.noreply.github.com>
    Co-authored-by: Jeffery Richardson <104876722+jrichardson-rn@users.noreply.github.com>
    Co-authored-by: Melanie Forbes <50688150+mforbz12@users.noreply.github.com>
    Co-authored-by: Elise McConnell<112969060+enmcco@users.noreply.github.com>
    Co-authored-by: Emily Paine<22458780+erpaine@users.noreply.github.com>

---
+++ __tests__/test components/dashboard.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {render, screen, fireEvent} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+import Dashboard from '../../client/pages/Dashboard';
+import { describe, it, expect, xdescribe, beforeEach, beforeAll, test} from'@jest/globals';
+
+describe('Dashboard Page', () => {
+
+  let dashboardComp;
+  const props = {}
+
+  beforeEach(async () => {
+    const loginComp = await render(
+      <BrowserRouter>
+        <Dashboard {...props}/>
+      </BrowserRouter>
+    );
+  });
+  test('if no DB has been connected, button prompts connecting one', async() => {
+    expect(screen.getByText('Please connect a database')).toBeInTheDocument()
+  })
+  //if dbURI = testdb, Tab List length should be 1
+  
+});
