9dd553f10d6e2894a3f05eec0e9508ff1d9d4fd4
Brandon Miller <Brandon@MacBook-Pro.local>
2023-03-08 15:33:18 -0800

    adjusting docker config for EB deployment

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ import express, {
 
 const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || '3000';
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ app.use(urlencoded({ extended: true }));
 app.use('/api', metricAPI);
 
 // serves static files in production mode
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
   app.use('/build', express.static(path.join(__dirname, '../build')));
   app.get('/*', (req, res) => {
     return res
@@ -39,8 +39,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server started on Port: ${port}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}...`);
 });
 
 module.exports = server;
